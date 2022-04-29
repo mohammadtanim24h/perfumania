0d68de4a3bbb35a2848177011774df9b69fa3fc6
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-04-29 17:50:45 +0600

    made banner responsive

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,14 +8,14 @@ const Header = () => {
         <div className="header">
             <Navbar className="custom-nav" collapseOnSelect expand="lg" bg="light">
                 <Container className="nav-container text-center">
-                    <Navbar.Brand as={Link} to="/">PERFUMANIA</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Brand className="d-md-none" as={Link} to="/">PERFUMANIA</Navbar.Brand>
+                    <Navbar.Toggle style={{ border: "1px solid #fff", backgroundColor: '#fff' }} aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="me-auto">
+                        <Nav className="ms-auto">
                             <Nav.Link as={Link} to="home">Home</Nav.Link>
                             <Nav.Link as={Link} to="about">About</Nav.Link>
                         </Nav>
-                        <Nav>
+                        <Nav className="me-auto">
                             <Nav.Link as={Link} to="login">Login</Nav.Link>
                             <Nav.Link as={Link} to="user">
                                 User
@@ -26,8 +26,9 @@ const Header = () => {
             </Navbar>
             <div className="banner-text text-center">
                 <h2 className="my-2">Perfumania</h2>
-                <h4 className="my-2">Your Perfect Stop for Fragrance</h4>
-                <p className="my-2">A perfect Theme to build beautiful and unique Perfume store eCommerce websites. It comes with nice and clean design.</p>
+                <h4 className="my-2">The Largest Perfume Warehouse</h4>
+                <p className="my-2">We have the largest warehouse of perfumes in the whole world. The collection we have here is next level.</p>
+                <button className="explore-btn rounded-pill">Explore</button>
             </div>
         </div>
     );
