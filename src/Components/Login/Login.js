20bfbb6ccbb413d8e6b9194ad8ee5d30c3079d24
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-04-30 15:26:27 +0600

    more login styles added

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Login.css";
 import { useForm } from "react-hook-form";
+import { GiPartyPopper } from "react-icons/gi";
+import google from '../../images/icons8-google.png';
+import facebook from '../../images/icons8-facebook.png';
+import github from '../../images/icons8-github.png';
 
 const Login = () => {
     const { register, handleSubmit } = useForm();
@@ -22,9 +26,18 @@ const Login = () => {
                             <div style={{width: '40%', height: '1px', backgroundColor: 'lightgray'}} ></div>
                         </div>
                     </form>
+                    <div className="my-3">
+                        <button className="d-block mx-auto mb-2 social-btn"> <img width={36} src={google} alt="" /> Sign in with Google</button>
+                        <button className="d-block mx-auto mb-2 social-btn"> <img width={36} src={facebook} alt="" /> Sign in with Facebook</button>
+                        <button className="d-block mx-auto mb-2 social-btn"> <img width={36} src={github} alt="" /> Sign in with Github</button>
+                    </div>
                 </div>
                 <div className="login-right">
-                    <h2 className="text-center">Welcome Back</h2>
+                    <div>
+                        <h3 className="text-center mt-3">Welcome Back <GiPartyPopper className="mb-2"></GiPartyPopper> </h3>
+                        <p className="text-center">Login using Email and Password.</p>
+                        <p className="text-center">Enter your personal details and continue your journey with us.</p>
+                    </div>
                 </div>
             </div>
         </div>
