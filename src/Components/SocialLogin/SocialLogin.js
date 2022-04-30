8a0f71029757b283e71bcb1887cc931bfd4dca7d
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-04-30 16:43:14 +0600

    added github sign in

--- src/Components/SocialLogin/SocialLogin.js
+++ src/Components/SocialLogin/SocialLogin.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../Firebase/firebase.init';
 import google from '../../images/icons8-google.png';
 import facebook from '../../images/icons8-facebook.png';
@@ -7,10 +7,13 @@ import github from '../../images/icons8-github.png';
 
 const SocialLogin = () => {
     // Google Sign in
-    const [signInWithGoogle, user] = useSignInWithGoogle(auth);
-    
-    // Github Sign in 
+    const [signInWithGoogle, userGoogle, , errorGoogle] = useSignInWithGoogle(auth);
 
+    // Github Sign in 
+    const [signInWithGithub, userGit, , errorGit] = useSignInWithGithub(auth);
+    if(userGit) {
+        console.log(userGit);
+    }
     return (
         <div>
             <div className="d-flex justify-content-center align-items-center mx-auto mt-3" style={{width: '80%'}}>
@@ -19,9 +22,11 @@ const SocialLogin = () => {
                 <div style={{width: '40%', height: '1px', backgroundColor: 'lightgray'}} ></div>
             </div>
             <div className="my-3">
+                {errorGit && <p className='text-center text-danger'>Error: {errorGit.message.slice(16, )}</p> }
+                {errorGoogle && <p className='text-center text-danger'>Error: {errorGoogle.message.slice(16, )}</p> }
                 <button onClick={() => signInWithGoogle()} className="d-block mx-auto mb-2 social-btn"> <img width={36} src={google} alt="" /> Sign in with Google</button>
                 <button className="d-block mx-auto mb-2 social-btn"> <img width={36} src={facebook} alt="" /> Sign in with Facebook</button>
-                <button className="d-block mx-auto mb-2 social-btn"> <img width={36} src={github} alt="" /> Sign in with Github</button>
+                <button onClick={() => signInWithGithub()} className="d-block mx-auto mb-2 social-btn"> <img width={36} src={github} alt="" /> Sign in with Github</button>
             </div>
         </div>
     );
