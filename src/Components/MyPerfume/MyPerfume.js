0b1912ff2a2dd8cfa5fbd1c1dbb5bb7a570e4fdf
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-05-06 15:38:25 +0600

    added update btn in my items page

--- src/Components/MyPerfume/MyPerfume.js
+++ src/Components/MyPerfume/MyPerfume.js
@@ -3,16 +3,18 @@ import { useNavigate } from 'react-router-dom';
 
 const MyPerfume = ({perfume, handleDelete}) => {
     const { name, img, description, price, supplier, quantity, _id } = perfume;
+    const navigate = useNavigate();
     return (
         <div className="col-lg-6">
-            <div className="perfume-card">
+            <div className="perfume-card exclude">
                 <div className="perfume-info">
                     <h4 className="text-secondary">{name}</h4>
                     <p>Price: ${price}</p>
                     <p>Quantity: {quantity}</p>
                     <p>{description.length > 133 ? description.slice(0, 133) + '...' : description}</p>
                     <p>Supplier: {supplier}</p>
-                    <button className='theme-btn rounded-pill' onClick={() => handleDelete(_id)}>Delete</button>
+                    <button onClick={() => navigate(`/inventory/${_id}`)} className="update-btn rounded-pill">Update</button>
+                    <button className='theme-btn rounded-pill ms-2' onClick={() => handleDelete(_id)}>Delete</button>
                 </div>
                 <img className="img-fluid" src={img} alt="" />
             </div>
