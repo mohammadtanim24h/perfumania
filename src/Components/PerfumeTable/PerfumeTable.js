80849c8ec31e785e5b4b05ece3bafe4ff89f4619
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-05-07 15:16:41 +0600

    added update button in manage inventories table

--- src/Components/PerfumeTable/PerfumeTable.js
+++ src/Components/PerfumeTable/PerfumeTable.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import './PerfumeTable.css';
 
 const PerfumeTable = ({perfume, handleDelete}) => {
     const {_id, name, price, quantity, sold} = perfume;
+    const navigate = useNavigate();
     return (
         <tr>
             <td>{name}</td>
@@ -10,7 +12,15 @@ const PerfumeTable = ({perfume, handleDelete}) => {
             <td>{quantity}</td>
             <td>{sold}</td>
             <td className="text-center">
-                <button onClick={() => handleDelete(_id)} className="delete-btn"><i style={{color: '#FF454C', fontSize: '24px'}} className="fa-solid fa-trash"></i></button>
+                <div>
+                    <div className="mb-1">
+                        <button onClick={() => handleDelete(_id)} className="delete-btn"><i style={{color: '#FF454C', fontSize: '24px'}} className="fa-solid fa-trash"></i></button>
+                    </div>
+                    <div style={{height: '1px', backgroundColor: 'lightgray'}}></div>
+                    <div className="mt-2">
+                        <button onClick={() => navigate(`/inventory/${_id}`)} className="btn-update-td rounded-pill">Update</button>
+                    </div>
+                </div>
             </td>
         </tr>
     );
